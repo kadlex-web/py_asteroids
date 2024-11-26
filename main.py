8ada08fe5ee686c6a1b9fd2da0f06a2d0cf05257
kadlex <akulp18@gmail.com>
2024-11-25 20:11:26 -0500

    updated main.py to generate game window

--- main.py
+++ main.py
@@ -2,13 +2,20 @@
 # the open-source pygame library
 # throughout this file
 import pygame
-import constants
+from constants import *
 
 def main():
+    pygame.init()
     print("Starting asteroids!")
-    print(f"Screen width: {constants.SCREEN_WIDTH}")
-    print(f"Screen height: {constants.SCREEN_HEIGHT}")
-
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return        
+        pygame.Surface.fill(screen, (0,0,0))
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
