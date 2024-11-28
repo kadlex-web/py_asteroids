27ce6787614c64241201fe679129fb633a88a8e7
kadlex <akulp18@gmail.com>
2024-11-27 19:32:31 -0500

    create player.py to define the player object for the game; refactored main.py slightly

--- main.py
+++ main.py
@@ -3,12 +3,14 @@
 # throughout this file
 import pygame # type: ignore
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
     dt = 0
     # Begins the game loop, draws a black screen based on the screen width and screen height constants
     while True:
@@ -16,7 +18,8 @@ def main():
             if event.type == pygame.QUIT:
                 return
         dt = clock.tick(60)/1000
-        pygame.Surface.fill(screen, (0,0,0))
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
 
 if __name__ == "__main__":
